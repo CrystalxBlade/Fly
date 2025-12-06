44a2c70365eb93ea76acade27fa2188664423f18
CrystalxBlade <satish141m@gmail.com>
2025-12-06 16:22:49 +0530

    trigger

--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -1,13 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreCounter : MonoBehaviour
 {
+    public int score;
+    public Text scoreText; 
     void Start()
     {
-        
+        score = 0;
     }
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("counter"))
+        {
+            score++;
+            scoreText.text = score.ToString();
+        }
     }
 }
