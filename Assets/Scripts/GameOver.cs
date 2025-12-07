37ba17019ec8c592f6ec58932f60732093482996
CrystalxBlade <satish141m@gmail.com>
2025-12-07 19:02:15 +0530

    new

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
-    
+    public Text currScore;
+    public Text highScore;
+
+    private void OnEnable()
+    {
+        
+    }
 }
