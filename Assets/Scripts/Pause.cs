12025f5a93af682989387538c5857d1407b06a36
CrystalxBlade <satish141m@gmail.com>
2025-12-12 19:05:45 +0530

    new

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -10,8 +10,13 @@ public class Pause : MonoBehaviour
     {
         Time.timeScale = 1;
     }
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
     public void Menu()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(0);
     }
 }
