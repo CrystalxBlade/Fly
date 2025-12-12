65562f6d76454b1eb99f65a4905a9307f3a56dd0
CrystalxBlade <satish141m@gmail.com>
2025-12-12 17:30:36 +0530

    new

--- Assets/Scripts/Home.cs
+++ Assets/Scripts/Home.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class Home : MonoBehaviour
 {
+    [SerializeField] Text scoreText; 
     void Start()
     {
-        
+        scoreText.text = PlayerPrefs.GetInt("HighScore").ToString();
     }
-
     public void Play()
     {
         SceneManager.LoadScene(1);
