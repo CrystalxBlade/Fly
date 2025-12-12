e1d0dc45391fa39069257c642a8070bbb2436881
CrystalxBlade <satish141m@gmail.com>
2025-12-12 17:36:14 +0530

    Scene

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Pause : MonoBehaviour
 {
-    public void  pauseGame()
+    public void  PauseGame()
     {
-        
+        Time.timeScale = 0;
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
